3985d266229884d288b84952ecba68548e7354db
FedePach <fm.pachec@gmail.com>
2024-01-26 16:09:48 -0300

    Estructura del proyecto

--- src/main/java/com/fedefactory/personaMicroService/PersonaMicroServiceApplication.java
+++ src/main/java/com/fedefactory/personaMicroService/PersonaMicroServiceApplication.java
@@ -3,11 +3,18 @@ package com.fedefactory.personaMicroService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.text.html.Option;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootApplication
 public class PersonaMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonaMicroServiceApplication.class, args);
 	}
+
 
 }
