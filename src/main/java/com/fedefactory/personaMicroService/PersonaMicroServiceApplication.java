6c7e1294631b38e40ad8cab55044914171b98416
FedeNotebook <fm.pachec@gmail.com>
2024-01-26 09:00:33 -0300

    Initializacion of the project

---
+++ src/main/java/com/fedefactory/personaMicroService/PersonaMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fedefactory.personaMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedefactory/personaMicroService/PersonaMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedefactory.personaMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
